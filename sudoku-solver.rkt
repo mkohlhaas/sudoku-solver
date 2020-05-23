a0339c8f8ea81b34aaa3be28eed0b0981be4d526
Hans-J. Schmid <knock@myopendoor.de>
2020-05-23 19:38:26 +0200

    first working version.

--- sudoku-solver.rkt
+++ sudoku-solver.rkt
@@ -1,8 +1,127 @@
 #lang racket
 
-; https://bit.ly/3ef2VPJ
-(define b1 '(0 1 6 4 0 0 0 0 0 2 0 0 0 0 9 0 0 0 4 0 0 0 0 0 0 6 2 0 7 0 2 3 0 1 0 0 1 0 0 0 0 0 0 0 3 0 0 3 0 8 7 0 4 0 9 6 0 0 0 0 0 0 5 0 0 0 8 0 0 0 0 7 0 0 0 0 0 6 8 2 0))
+(provide (all-defined-out))
 
+; https://bit.ly/3ef2VPJ
+; listof(val)
+(define b1 
+  '( 0 1 6 4 0 0 0 0 0 
+     2 0 0 0 0 9 0 0 0 
+     4 0 0 0 0 0 0 6 2 
+     0 7 0 2 3 0 1 0 0 
+     1 0 0 0 0 0 0 0 3 
+     0 0 3 0 8 7 0 4 0 
+     9 6 0 0 0 0 0 0 5 
+     0 0 0 8 0 0 0 0 7 
+     0 0 0 0 0 6 8 2 0 ))
+
+(define b2
+  '( 0 4 9 0 0 8 6 0 5 
+     0 0 3 0 0 7 0 0 0 
+     0 0 0 0 0 0 0 3 0 
+     0 0 0 4 0 0 8 0 0 
+     0 6 0 8 1 5 0 2 0 
+     0 0 1 0 0 9 0 0 0 
+     0 1 0 0 0 0 0 0 0 
+     0 0 0 6 0 0 4 0 0 
+     8 0 4 5 0 0 3 9 0 ))
+
+(define b3
+  '( 7 6 0 5 0 0 0 0 0 
+     0 0 0 0 6 0 0 0 8 
+     0 0 0 0 0 0 4 0 3 
+     2 0 0 4 0 0 8 0 0 
+     0 8 0 0 0 0 0 3 0 
+     0 0 5 0 0 1 0 0 7 
+     8 0 9 0 0 0 0 0 0 
+     6 0 0 0 1 0 0 0 0 
+     0 0 0 0 0 3 0 4 1 ))
+
+(define b4
+  '( 0 0 0 6 0 5 0 0 0 
+     0 0 3 0 2 0 8 0 0 
+     0 4 5 0 9 0 2 7 0 
+     5 0 0 0 0 0 0 0 1 
+     0 6 2 0 0 0 5 4 0 
+     4 0 0 0 0 0 0 0 7 
+     0 9 8 0 6 0 4 5 0 
+     0 0 6 0 4 0 7 0 0 
+     0 0 0 2 0 3 0 0 0 ))
+
+(define b5
+  '(
+    4 0 9 0 0 0 7 0 5 
+    0 0 0 0 1 0 0 0 0 
+    0 0 6 2 0 7 8 0 0 
+    2 0 0 0 0 0 0 0 9 
+    0 0 3 7 0 4 2 0 0 
+    8 0 0 0 0 0 0 0 4 
+    0 0 2 8 0 1 5 0 0 
+    0 0 0 0 6 0 0 0 0 
+    9 0 5 0 0 0 4 0 6 ))
+
+(define b6
+  '(
+    0 0 0 0 1 0 0 3 0 
+    0 4 0 0 7 0 5 0 1 
+    0 0 2 0 0 8 0 0 6 
+    6 8 0 0 0 0 0 0 3 
+    0 0 0 3 0 2 0 0 0 
+    3 0 0 0 0 0 0 4 5 
+    2 0 0 5 0 0 8 0 0 
+    8 0 1 0 4 0 0 2 0 
+    0 9 0 0 2 0 0 0 0 ))
+
+(define b7
+  '(
+    0 8 0 0 7 0 0 3 0 
+    2 6 0 0 5 0 0 1 8 
+    0 0 0 0 0 0 4 0 0 
+    0 0 0 6 0 2 0 0 0 
+    3 9 0 0 1 0 0 8 6 
+    0 0 0 7 0 9 0 0 0 
+    0 0 4 0 0 0 8 0 0 
+    8 1 0 0 4 0 0 5 2 
+    0 5 0 0 9 0 0 7 0 ))
+
+(define b8
+  '(
+    0 0 0 0 9 3 0 0 6 
+    0 0 0 8 0 0 9 0 0 
+    0 2 0 0 0 6 1 0 0 
+    0 0 0 0 8 0 0 5 3 
+    0 0 6 0 0 0 2 0 0 
+    3 7 0 0 5 0 0 0 0 
+    0 0 2 5 0 0 0 4 0 
+    0 0 1 0 0 9 0 0 0 
+    7 0 0 1 3 0 0 0 0 ))
+
+; Gordon Royle's web page: https://bit.ly/2WXNExd
+; See also sudoko17 text file in the repo.
+(define b9
+  '( 0 0 0 0 0 0 0 1 0 
+     4 0 0 0 0 0 0 0 0 
+     0 2 0 0 0 0 0 0 0 
+     0 0 0 0 5 0 4 0 7 
+     0 0 8 0 0 0 3 0 0 
+     0 0 1 0 9 0 0 0 0 
+     3 0 0 4 0 0 2 0 0 
+     0 5 0 1 0 0 0 0 0 
+     0 0 0 8 0 6 0 0 0 ))
+
+(define b10
+  '(
+    0 0 0 0 0 0 0 1 0 
+    4 0 0 0 0 0 0 0 0 
+    0 2 0 0 0 0 0 0 0 
+    0 0 0 0 5 0 6 0 4 
+    0 0 8 0 0 0 3 0 0 
+    0 0 1 0 9 0 0 0 0 
+    3 0 0 4 0 0 2 0 0 
+    0 5 0 1 0 0 0 0 0 
+    0 0 0 8 0 7 0 0 0 ))
+
+; listof(listof(pos))
 (define rows 
   '((  0  1  2  3  4  5  6  7  8 ) 
     (  9 10 11 12 13 14 15 16 17 ) 
@@ -14,6 +133,7 @@
     ( 63 64 65 66 67 68 69 70 71 ) 
     ( 72 73 74 75 76 77 78 79 80 )))
 
+; listof(listof(pos))
 (define cols 
   '(( 0  9 18 27 36 45 54 63 72 )
     ( 1 10 19 28 37 46 55 64 73 )
@@ -25,6 +145,7 @@
     ( 7 16 25 34 43 52 61 70 79 )
     ( 8 17 26 35 44 53 62 71 80 )))
 
+; listof(listof(pos))
 (define squares 
   '((  0  1  2  9 10 11 18 19 20 )
     (  3  4  5 12 13 14 21 22 23 )
@@ -36,63 +157,76 @@
     ( 57 58 59 66 67 68 75 76 77 )
     ( 60 61 62 69 70 71 78 79 80 )))
 
-; board -> board | false
-(define (solve-b b)
-  (if (solved? b)
-      b
-      (solve-lob (create-new-boards b))))
+; listof(listof(pos))
+(define blocks
+  (append rows cols squares))
 
 ; board -> board | false
-(define (solve-lob lob)
-  (cond [(empty? log) #f]
-	[else
-	  (or (solve-b (first lob))
-	      (solve-lob (rest lob)))]))
+(define (solve-sudoku b)
+  ; board -> board | false
+  (define (solve-b b)
+    (if (solved? b)
+	b
+	(solve-lob (create-new-boards b))))
+
+  ; board -> board | false
+  (define (solve-lob lob)
+    (cond [(empty? lob) #f]
+	  [else
+	    (or (solve-b (first lob))
+		(solve-lob (rest lob)))]))
+  (solve-b b))
 
 ; board -> bool
 (define (solved? b)
-  (not (or-map zero? b)))
+  (not (ormap zero? b)))
 
 ; board -> listof(board)
 (define (create-new-boards b)
-  (let [next-pos (find-next-hole b)]
-    (keep-valid-boards
-      (new-boards b next-pos)
-      next-pos)))
+  (let ([next-pos (find-next-hole b)])
+    (new-boards b next-pos)))
 
 ; board -> pos
+; board = listof(pos)
+; TODO: here you can insert any kind of heuristics, eg. hole with the most
+; hints; new hole that is furthest away from previous hole, etc.
 (define (find-next-hole b)
   (- (length b)
      (length (member 0 b))))
 
 ; board pos -> listof(board)
 (define (new-boards b p)
-  (build-list 9 (lambda(i)
-		  (replace-board-value b p (add1 i)))))
+  (filter (lambda(b) (not (false? b))) ; filter out invalid new boards
+	  (build-list 9 (lambda(i)
+			  (new-board-or-false b p (add1 i))))))
 
-; listof(board) pos -> listof(board)
-(define (keep-valid-boards lob pos))
+; board pos value -> board | #f
+(define (new-board-or-false b p v)
+  (let* ([new-board (append (take b p)			   			; listof(pos)
+			    (list v)
+			    (drop b (add1 p)))]
+	 [lob (get-valid-blocks p)]				      		; listof 3 blocks = listof(listof(pos))
+	 [lolov (map (lambda(block)				     		; listof(listof(val)) -- (size = 3); block = listof(pos)
+		       (map (lambda(p) (list-ref new-board p))
+			    block))
+		     lob)]
+	 [lolov-wo-zeros (map (lambda(lov)					; listof(listof(val)) -- (size = 3) 
+				(filter (lambda(v)			
+					  (not (zero? v)))
+					lov))
+			      lolov)]
+	 [valid-new-board? (andmap (lambda(lov)
+				     (= (length lov) 
+					(length (remove-duplicates lov))))
+				   lolov-wo-zeros)])
+    (if valid-new-board?
+	new-board
+	'#f)))
 
-; board pos value -> board
-(define (replace-board-value b p v)
-  (append (take b p)
-	  (list v)
-	  (drop b (add1 pos))))
+; pos -> listof(listof(pos))
+; should be a list of three blocks - one for each row, col, square 
+(define (get-valid-blocks p)
+  (filter (lambda(lop) (member p lop))
+	  blocks))
 
-; board pos -> bool
-(define (check-sudoku-rules b p)
-  (let ([row (get-row p)]
-	[col (get-col p)]
-	[squ (get-square p)])
-    (check-rows b)
-    (check-cols b)
-    (check-squares b)))
-
-; pos -> listof(pos)
-(define (get-row p))
-
-; pos -> listof(pos)
-(define (get-col p))
-
-; pos -> listof(pos)
-(define (get-square p))
+; (solve-sudoku b1)
