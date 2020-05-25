57f904863cb313dc2055f9fbea5b1683a123d2fa
Hans-J. Schmid <knock@myopendoor.de>
2020-05-25 05:10:59 +0200

    reorganizes files and directories.

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define collection "sudoku-solver")
+(define build-deps '("rackunit-lib"))
+(define pkg-desc "Solves sudoku puzzles.")
+(define version "0.1")
+(define pkg-authors '(schmid))
